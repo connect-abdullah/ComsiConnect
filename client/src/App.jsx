534bf37dd825930e2e50af36fbbac236cddd4253
M Abdullah <150430204+connect-abdullah@users.noreply.github.com>
2025-05-15 22:36:36 +0500

    saved changes

--- client/src/App.jsx
+++
@@ -1,8 +0,0 @@
-import FollowList from './pages/profileSection/FollowList';
-
-// In your router configuration:
-<Routes>
-  {/* ... existing routes ... */}
-  <Route path="/profile/:userId/:type" element={<FollowList />} />
-</Routes>
-// ... existing code ... 
