c9753ef8b6d6f65c890d2101fa1dd0efd1fb35ff
M Abdullah <150430204+connect-abdullah@users.noreply.github.com>
2025-06-04 22:12:01 +0500

    changed server

--- cilent/src/api/api.js
+++ cilent/src/api/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = "http://localhost:8080"; // /api 
+const API_URL = "/api"; // /api 
 // const API_URL = "http://localhost:3000";
 
 const api = axios.create({
